fe81e226df1ff503db18fb0b21425cda0a6ef9bd
0220832111004 <0220832111004@LAB4TC28>
2022-02-23 09:52:37 -0300

    Adicionar arquivos de projeto.

---
+++ Class 01/Controllers/PessoaController.cs
@@ -0,0 +1,30 @@
+﻿using Class_01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class_01.Controllers
+{
+    public class PessoaController : Controller
+    {
+        public static List<Pessoa> lsPessoa = new List<Pessoa>();
+        public IActionResult Index()
+        {
+            return View(lsPessoa);
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public IActionResult Salvar(Pessoa itemA)
+        {
+            lsPessoa.Add(itemA);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Class 01/Models/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class_01.Models
+{
+    public class Pessoa
+    {
+        public string nome { get; set; }
+        public int idade { get; set; }
+    }
+}
